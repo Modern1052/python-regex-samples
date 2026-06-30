48ac9b0d485b82df0da4648241856e9c764e79b4
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 02:15:26 +0900

    Update 49_dot.py

--- 49_dot.py
+++ 49_dot.py
@@ -1,9 +1,7 @@
 import re
 
 pattern = r'.at'
-message = '''/
-The cat in the hat sat on the flat mat./
-'''
+message = 'The cat in the hat sat on the flat mat.'
 
 pattern_obj = re.compile(pattern)
 matches = pattern_obj.findall(message)
