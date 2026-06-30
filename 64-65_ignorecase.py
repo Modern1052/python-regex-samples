2882b342480fa262d82c22ac93b513855263772a
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 02:17:31 +0900

    Update 64-65_ignorecase.py

--- 64-65_ignorecase.py
+++ 64-65_ignorecase.py
@@ -2,10 +2,7 @@ import re
 
 pattern = r'robocop'
 flag = re.IGNORECASE
-message = '''\
-RoboCop is part man, \
-part machine, all cop.\
-'''
+message = 'RoboCop is part man, part machine, all cop.'
 
 pattern_obj = re.compile(pattern, flag)
 match_obj = pattern_obj.search(message)
