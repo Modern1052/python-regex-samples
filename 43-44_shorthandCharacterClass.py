2646e6f5b769700e30fe77234269bc48f5a11003
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 02:14:39 +0900

    Update 43-44_shorthandCharacterClass.py

--- 43-44_shorthandCharacterClass.py
+++ 43-44_shorthandCharacterClass.py
@@ -1,10 +1,7 @@
 import re
 
 pattern = r'\d+\s\w+'
-message ='''\
-12 drummers, 11 pipers, 10 lords, \
-9 ladies, 8 maids, 7 swans, ...\
-'''
+message = '12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, ...'
 
 pattern_obj = re.compile(pattern)
 matches = pattern_obj.findall(message)
