cc9318ce5518a8733742d1422541ba509952576a
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 02:17:02 +0900

    Update 60-62_startsAndEnds.py

--- 60-62_startsAndEnds.py
+++ 60-62_startsAndEnds.py
@@ -1,12 +1,19 @@
 import re
 
-pattern = r'(Ha){3,5}'
-#pattern = r'(Ha){3,5}?'
-#pattern = r'(Ha)+'
-#pattern = r'(Ha)+?'
-message = 'HaHaHaHaHaHa'
+#pattern = r'\d+'
+pattern = r'^\d+'
+#pattern = r'\d+$'
+messages = (
+    'I have 3 cats.',
+    '10 years ago, ...',
+    'password : 2336'
+)
 
 pattern_obj = re.compile(pattern)
-match_obj = pattern_obj.search(message)
-match = match_obj.group()
-print(match)
+for message in messages:
+    match_obj = pattern_obj.search(message)
+    if match_obj:
+        match = match_obj.group()
+        print(match)
+    else:
+        print('None')
