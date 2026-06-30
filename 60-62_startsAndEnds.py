c768571a0aa04cd9641b7bc627a1e74b94d16883
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 02:05:59 +0900

    Create 60-62_startsAndEnds.py

---
+++ 60-62_startsAndEnds.py
@@ -0,0 +1,12 @@
+import re
+
+pattern = r'(Ha){3,5}'
+#pattern = r'(Ha){3,5}?'
+#pattern = r'(Ha)+'
+#pattern = r'(Ha)+?'
+message = 'HaHaHaHaHaHa'
+
+pattern_obj = re.compile(pattern)
+match_obj = pattern_obj.search(message)
+match = match_obj.group()
+print(match)
