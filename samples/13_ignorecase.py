4a1768ef33f0fd04b291ef7517516822d280eecf
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 10:22:17 +0900

    Create 13_ignorecase.py

---
+++ samples/13_ignorecase.py
@@ -0,0 +1,10 @@
+import re
+
+pattern = r'robocop'
+flag = re.IGNORECASE
+message = 'RoboCop is part man, part machine, all cop.'
+
+pattern_obj = re.compile(pattern, flag)
+match_obj = pattern_obj.search(message)
+match = match_obj.group()
+print(match)
