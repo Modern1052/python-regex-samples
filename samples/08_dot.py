e264de46bd085720bcd5ee99c321adf0f35977f2
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 10:18:59 +0900

    Create 08_dot.py

---
+++ samples/08_dot.py
@@ -0,0 +1,9 @@
+import re
+
+pattern = r'.at'
+message = 'The cat in the hat sat on the flat mat.'
+
+pattern_obj = re.compile(pattern)
+matches = pattern_obj.findall(message)
+for match in matches:
+    print(match)
