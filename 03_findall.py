1cc3f2abe9aad0c349b1e3efa12f6bb46d8d350b
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 10:25:37 +0900

    Delete 03_findall.py

--- 03_findall.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern = r'\d{3}-\d{3}-\d{4}'
-message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
-
-pattern_obj = re.compile(pattern)
-matches = pattern_obj.findall(message)
-for match in matches:
-    print(match)
