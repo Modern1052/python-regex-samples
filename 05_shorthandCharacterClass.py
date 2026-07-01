d9a3bf18e5603cfcb7db545aa93c588662e6fb16
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 10:25:56 +0900

    Delete 05_shorthandCharacterClass.py

--- 05_shorthandCharacterClass.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern = r'\d+\s\w+'
-message = '12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, ...'
-
-pattern_obj = re.compile(pattern)
-matches = pattern_obj.findall(message)
-for match in matches:
-    print(match)
