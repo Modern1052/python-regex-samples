f3eca67e328f2474d7eb620d10efd5f2782dda96
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 10:26:18 +0900

    Delete 08_dot.py

--- 08_dot.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern = r'.at'
-message = 'The cat in the hat sat on the flat mat.'
-
-pattern_obj = re.compile(pattern)
-matches = pattern_obj.findall(message)
-for match in matches:
-    print(match)
