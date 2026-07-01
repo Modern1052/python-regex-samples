3c67cd881e5a49ed923d3e80bf98053555ea900e
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 10:18:10 +0900

    Create 06_pipe.py

---
+++ samples/06_pipe.py
@@ -0,0 +1,9 @@
+import re
+
+pattern = r'dog|cat'
+message = 'I have a cat. My friend has a dog.'
+
+pattern_obj = re.compile(pattern)
+matches = pattern_obj.findall(message)
+for match in matches:
+    print(match)
