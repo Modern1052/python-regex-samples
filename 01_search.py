f59fe452008160e12acb74b8bb44e6684886e997
Modern1052 <yuya_shimoda.2@icloud.com>
2026-07-01 10:24:52 +0900

    Delete 01_search.py

--- 01_search.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern = r'\d{3}-\d{3}-\d{4}'
-message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
-
-pattern_obj = re.compile(pattern)
-match_obj = pattern_obj.search(message)
-match = match_obj.group()
-print(match)
